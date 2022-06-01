fa1aafb897077f7ecc4355f90b89564322484420
jmahajan117 <jmahajan@DESKTOP-MLKOGHT.localdomain>
2022-06-01 15:26:19 -0500

    Started data collecting algo

--- dataLoader.py
+++ dataLoader.py
@@ -1,10 +1,32 @@
 import numpy as np
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import json
 
 
+finalList = []
 
-class DataLoader:
-    def __init__(self):
-        pass
+def main():
+    credentials = json.load(open("secret.json"))
+    client_id = credentials["client_id"]
+    client_secret = credentials["client_secret"]
 
-    def collect(self):
-        pass
+    playlist_idx = 0
+    uri = "https://open.spotify.com/playlist/37i9dQZF1DWWQRwui0ExPn?si=5dd129e4b18043b5"
+
+    credit_manager = SpotifyClientCredentials(client_id, client_secret)
+    sp = spotipy.Spotify(client_credentials_manager=credit_manager)
+    while(playlist_idx != 1000):
+        results = sp.user_playlist_tracks("Spotify", uri, offset=playlist_idx)
+        extractData(results)
+        playlist_idx += 100
+    print(len(finalList))
+
+
+def extractData(tracks):
+    for track in tracks["items"]:
+        finalList.append(track['track']['name'])
+
+
+if __name__ == "__main__":
+    main()
